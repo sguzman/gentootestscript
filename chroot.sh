9de27a6273683ec5ca02ca3b49dbc82a827596b1
Salvador Guzman <guzmansalv@gmail.com>
2020-06-09 21:22:05 -0700

    fixed locale - odnt gen all of them

--- chroot.sh
+++ chroot.sh
@@ -5,7 +5,6 @@ set -e
 source /etc/profile
 export PS1="(chroot) ${PS1}"
 
-mount /dev/sda1 /boot
 emerge-webrsync --verbose --debug
 
 eselect profile list
@@ -15,14 +14,12 @@ ls /usr/share/zoneinfo
 echo "America/Los_Angeles" > /etc/timezone
 emerge --verbose --config sys-libs/timezone-data
 
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
+echo 'LC_COLLATE="C"' >> /etc/env.d/02locale
 
-echo 'en_US ISO-8859-1' > /etc/locale.gen
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 eselect locale list
-
-echo 'LANG="de_DE.UTF-8"' > /etc/env.d/02locale
-echo 'LC_COLLATE="C"' >> /etc/env.d/02locale
 
 env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
 
