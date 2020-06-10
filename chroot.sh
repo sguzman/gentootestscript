cde624eb3c4162dac850975c9c5d9f5a3e445b86
Salvador Guzman <guzmansalv@gmail.com>
2020-06-10 01:55:10 -0700

    no whitespace

--- chroot.sh
+++ chroot.sh
@@ -15,7 +15,6 @@ emerge --verbose --config sys-libs/timezone-data
 
 eselect profile list
 
-
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
 echo 'LC_COLLATE="C"' >> /etc/env.d/02locale
